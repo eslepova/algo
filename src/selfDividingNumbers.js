2b487f603917eaed6c2cdfe681f035097f75bb39
Екатерина М. Слепова <slepova@slepova-mac.yamoney.ru>
2020-05-09 18:21:35 +0300

    add selfDividingNumbers

---
+++ src/selfDividingNumbers.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+const selfDividingNumbers = (left, right) => {
+    const result = [];
+
+    for(let num = left; num <= right; ++num) {
+        const digits = num.toString().split('');
+        digits.every(digit => num % digit === 0)
+        && result.push(num);
+    }
+
+    return result;
+};
